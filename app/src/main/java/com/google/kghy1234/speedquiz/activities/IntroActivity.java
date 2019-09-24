3db184f00daf58fa4fbf71a248677a615a4a7619
kghy1234 <kghy1234@gmail.com>
2019-09-24 10:32:35 +0900

    Intro added

--- app/src/main/java/com/google/kghy1234/speedquiz/activities/IntroActivity.java
+++ app/src/main/java/com/google/kghy1234/speedquiz/activities/IntroActivity.java
@@ -3,12 +3,12 @@ package com.google.kghy1234.speedquiz.activities;
 import com.google.kghy1234.speedquiz.R;
 import com.google.kghy1234.speedquiz.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class IntroActivity extends BaseActivity {
 
 
     @Override
     protected int getLayoutResource() {
-        return R.layout.activity_main;
+        return R.layout.activity_intro;
     }
 
     @Override
